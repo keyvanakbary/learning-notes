f13cdf3f8f0a34d53649ed149dd1225327c01bb3
keyvanakbary <keyvanakbary@users.noreply.github.com>
2021-02-14 22:36:10 +0000

    deploy: d67d07537ed58a1602c9d8369586028dc90ab3e7

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -53,7 +53,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', '/https://keyvanakbary.github.io/learning-notes/assets/js/search-data.json', true);
+  request.open('GET', '/learning-notes/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -430,7 +430,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', '/https://keyvanakbary.github.io/learning-notes/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', '/learning-notes/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready
